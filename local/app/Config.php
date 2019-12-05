1aa266a4c014193659d8b7b65a24acc14f4c037d
akorotkov-developer <v9065267799@gmail.com>
2019-12-05 08:26:46 +0300

    Инициализация шаблона и удаление лишних файлов

---
+++ local/app/Config.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace RusHydro;
+
+class Config {
+    const RUSHYDRO_TEMPLATE_PATH = "/local/templates/metall_main/";
+}
---
+++ local/templates/rushydro_main/lang/ru/description.php
@@ -0,0 +1,4 @@
+<?php
+$MESS["CFST_TEMPLATE_NAME"] = "RusHydro главный шаблон";
+$MESS["CFST_TEMPLATE_DESC"] = "RusHydro главный шаблон";
+?>
