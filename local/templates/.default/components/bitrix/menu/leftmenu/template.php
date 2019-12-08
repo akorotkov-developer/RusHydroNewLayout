17fedbde3d815587cc247360b515eeecf4d2d632
akorotkov-developer <v9065267799@gmail.com>
2019-12-08 14:51:42 +0300

    Активные пункты для левого меню

--- local/templates/.default/components/bitrix/menu/leftmenu/template.php
+++ local/templates/.default/components/bitrix/menu/leftmenu/template.php
@@ -11,16 +11,37 @@
         <nav class="navigation">
             <ul class="navigation_list">
                 <?php foreach ($arResult["TREE_MENU"] as $arItem) {?>
-                    <?php if ($arItem["CHILDS"]) {?>
-                        <li class="navigation_li"><span class="navigation_link"><?=$arItem["TEXT"]?></span>
-                            <ul class="navigation_sub-list">
-                                <?php foreach ($arItem["CHILDS"] as $arChild) {?>
-                                    <li class="navigation_sub-li"><a href="<?=$arChild["LINK"]?>" class="navigation_sub-link">- <?=$arChild["TEXT"]?></a></li>
+                    <?php if ($arItem["CHILDS"]) {
+                        if ($arItem["SELECTED"]) {
+                            $activeClass = "-active";
+                            $activeStyle = "style = 'display: block;'";
+                        } else {
+                            $activeClass = "";
+
+                        }
+                        ?>
+                        <li class="navigation_li <?=$activeClass?>"><span class="navigation_link"><?=$arItem["TEXT"]?></span>
+                            <ul class="navigation_sub-list" <?=$activeStyle?>>
+                                <?php foreach ($arItem["CHILDS"] as $arChild) {
+                                    if ($arChild["SELECTED"]) {
+                                        $activeClass = "active";
+                                    } else {
+                                        $activeClass = "";
+                                    }
+                                    ?>
+                                    <li class="navigation_sub-li"><a href="<?=$arChild["LINK"]?>" class="navigation_sub-link <?=$activeClass?>">- <?=$arChild["TEXT"]?></a></li>
                                 <?php }?>
                             </ul>
                         </li>
-                    <?php } else {?>
-                        <li class="navigation_li"><span class="navigation_link"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></span></li>
+                    <?php } else {
+                        if ($arItem["SELECTED"]) {
+                            $activeClass = "-active";
+                        } else {
+                            $activeClass = "";
+
+                        }
+                        ?>
+                        <li class="navigation_li <?=$activeClass?>"><span class="navigation_link"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></span></li>
                     <?php }?>
                 <?php }?>
             </ul>
