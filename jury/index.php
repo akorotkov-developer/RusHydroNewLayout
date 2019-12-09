b79a900124bba35da28debbdef9ad856aee6543b
akorotkov-developer <v9065267799@gmail.com>
2019-12-09 12:23:22 +0300

    Контентные страницы

--- jury/index.php
+++ jury/index.php
@@ -4,6 +4,40 @@ $APPLICATION->SetTitle("Жюри");
 $APPLICATION->AddChainItem($APPLICATION->GetTitle());
 
 use RusHydro\Config;
-?>
-
+?><table>
+<tbody>
+<tr>
+	<td width="122" valign="top">
+		<p>
+			 Богуш Б.Б.		</p>	</td>
+	<td width="779" valign="top">
+		<p>
+			 —&nbsp;член Правления, первый заместитель Генерального директора – главный инженер, председатель жюри Конкурса;		</p>	</td>
+	</tr>
+<tr>
+	<td valign="top">
+		<p>
+			 Хмарин В.В.		</p>	</td>
+	<td valign="top">
+		<p>
+			 —&nbsp;член Правления, заместитель Генерального директора;		</p>	</td>
+	</tr>
+<tr>
+	<td valign="top">
+		<p>
+			 Первеева Б.Н.		</p>	</td>
+	<td valign="top">
+		<p>
+			 —&nbsp;заместитель Генерального директора по управлению персоналом и организационному развитию;		</p>	</td>
+	</tr>
+<tr>
+	<td valign="top">
+		<p>
+			 Нагога М.Г.		</p>	</td>
+	<td valign="top">
+		<p>
+			 —&nbsp;директор Департамента корпоративных коммуникаций.		</p>	</td>
+</tr>
+</tbody>
+</table>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
