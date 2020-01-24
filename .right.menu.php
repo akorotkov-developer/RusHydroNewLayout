476fdaf5a24ffcd572db6a280170d8c2bff335b8
akorotkov-developer <v9065267799@gmail.com>
2020-01-24 15:18:25 +0300

    Закачать новое меню

--- .right.menu.php
+++ .right.menu.php
@@ -1,34 +1,32 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-
 <?
-$aMenuLinks = [
-	[
-		"Подать заявку",
-		"/submitapplication/",
-		[],
-		[],
-		""
-    ],
-	[
+$aMenuLinks = Array(
+	Array(
+		"Подать заявку", 
+		"/submitapplication/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
 		"Конкурсные работы 2019",
-		"/competitivework/",
-		[],
-		[],
-		""
-    ],
-	[
-		"Задать вопрос",
-		"#win1",
-		[],
-		["orange_class" => "buttons_list-orang"],
-		""
-    ],
-	[
-		"Контакты",
-		"/contacts/",
-		[],
-		["orange_class" => "buttons_list-orang"],
-		""
-    ],
-];
+		"/competitivework/?year=2019",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Задать вопрос", 
+		"#win1", 
+		Array(), 
+		Array("orange_class"=>"buttons_list-orang"), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"/contacts/", 
+		Array(), 
+		Array("orange_class"=>"buttons_list-orang"), 
+		"" 
+	)
+);
 ?>
